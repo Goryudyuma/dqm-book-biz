809ee1645e456c035267a6dbab4ce1c1db8430c6
Goryudyuma <umaretekyoumade@gmail.com>
2018-10-27 02:36:20 +0900

    [REFACTOR] コメントアウトされていた良さげな文を使うようにした

--- src/Main.elm
+++ src/Main.elm
@@ -93,58 +93,15 @@ view model =
                     ]
                 ]
             ]
-        , tbody []
-            --  <|  List.map monsterViewModel2View
-            --        [ MonsterViewModel "スライム" "8" "0" "9" "4"
-            --        , MonsterViewModel "おおがらす" "9" "0" "10" "6"
-            --        , MonsterViewModel "さまようよろい" "55" "0" "47" "10"
-            --        , MonsterViewModel "ドルイド" "35" "10" "55" "29"
-            --        , MonsterViewModel "はぐれメタル" "6" "∞" "55" "150"
-            --        , MonsterViewModel "ゾーマ" "4700" "∞" "360" "80"
-            --      ]
-            [ tr []
-                [ td [] [ text "スライム" ]
-                , td [] [ text "8" ]
-                , td [] [ text "0" ]
-                , td [] [ text "9" ]
-                , td [] [ text "4" ]
+        , tbody [] <|
+            List.map monsterFieldView
+                [ MonsterViewModel "スライム" "8" "0" "9" "4"
+                , MonsterViewModel "おおがらす" "9" "0" "10" "6"
+                , MonsterViewModel "さまようよろい" "55" "0" "47" "10"
+                , MonsterViewModel "ドルイド" "35" "10" "55" "29"
+                , MonsterViewModel "はぐれメタル" "6" "∞" "55" "150"
+                , MonsterViewModel "ゾーマ" "4700" "∞" "360" "80"
                 ]
-            , tr []
-                [ td [] [ text "おおがらす" ]
-                , td [] [ text "9" ]
-                , td [] [ text "0" ]
-                , td [] [ text "10" ]
-                , td [] [ text "6" ]
-                ]
-            , tr []
-                [ td [] [ text "さまようよろい" ]
-                , td [] [ text "55" ]
-                , td [] [ text "0" ]
-                , td [] [ text "47" ]
-                , td [] [ text "10" ]
-                ]
-            , tr []
-                [ td [] [ text "ドルイド" ]
-                , td [] [ text "35" ]
-                , td [] [ text "10" ]
-                , td [] [ text "55" ]
-                , td [] [ text "29" ]
-                ]
-            , tr []
-                [ td [] [ text "はぐれメタル" ]
-                , td [] [ text "6" ]
-                , td [] [ text "∞" ]
-                , td [] [ text "55" ]
-                , td [] [ text "150" ]
-                ]
-            , tr []
-                [ td [] [ text "ゾーマ" ]
-                , td [] [ text "4700" ]
-                , td [] [ text "∞" ]
-                , td [] [ text "360" ]
-                , td [] [ text "80" ]
-                ]
-            ]
         ]
 
 
