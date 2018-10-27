b231754917d109d56b91ee382d6c780b458b7da2
Goryudyuma <umaretekyoumade@gmail.com>
2018-10-27 12:02:23 +0900

    [GREEN] 念の為、テストケースを追加

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -137,5 +137,22 @@ suite =
                 , Monster "スライム" 8 0 9 4
                 , Monster "はぐれメタル" 6 infinity 55 150
                 ]
+            , monsterSortTest
+                "すばやさが降順のテスト"
+                (Order Agility Dsc)
+                [ Monster "スライム" 8 0 9 4
+                , Monster "おおがらす" 9 0 10 6
+                , Monster "さまようよろい" 55 0 47 10
+                , Monster "ドルイド" 35 10 55 29
+                , Monster "はぐれメタル" 6 infinity 55 150
+                , Monster "ゾーマ" 4700 infinity 360 80
+                ]
+                [ Monster "はぐれメタル" 6 infinity 55 150
+                , Monster "ゾーマ" 4700 infinity 360 80
+                , Monster "ドルイド" 35 10 55 29
+                , Monster "さまようよろい" 55 0 47 10
+                , Monster "おおがらす" 9 0 10 6
+                , Monster "スライム" 8 0 9 4
+                ]
             ]
         ]
